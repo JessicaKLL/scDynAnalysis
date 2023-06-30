1cb00c8a70af83a9e6a22ce7a43226a252d08d3e
JessicaKLL <129850754+JessicaKLL@users.noreply.github.com>
2023-06-30 13:54:04 +0200

    Update plot_dyn.R

--- R/plot_dyn.R
+++ R/plot_dyn.R
@@ -3,6 +3,7 @@
 #' This function generates a plot representing the dynamics of the expression of one feature in different patients
 #'
 #' @param data Data
+#' @param spar Smoothing parameter
 #'
 #' @import ggplot2
 #' @import ggformula
@@ -15,9 +16,9 @@
 #' @export
 #'
 
-plot_dyn<-function(data){
+plot_dyn<-function(data,spar=0.5){
     p<-ggplot(data,aes(Meta_cells,Gene,group=Individual,col=Individual))+
       geom_point(alpha=0.07)+geom_line(alpha=0.07)+
-      geom_spline(size=1,spar = 0.7)
+      geom_spline(size=1,spar = spar)
   return(p)
 }
